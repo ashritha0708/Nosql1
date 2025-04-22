95425bee19b7558044cdab875f4252af21d6a30f
ashrithareddykommareddy <ashritha.97kommareddy@gmail.com>
2025-04-21 19:33:29 -0500

    Initial commit

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ const express = require('express');
 const app = express()
 const port = process.env.port || 8080
 const authRoute = require('./routes/auth-route')
+const bookRoute = require('./routes/booking-route')
+const CRUDroute = require('./routes/CRUD-route')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
 mongoose.connect(
-    "mongodb+srv://akshithaksh56:pass123@plan-your-trip.t4bsyph.mongodb.net/appData",
+    "mongodb+srv://ashritha97kommareddy:Ash13579@cluster0.ggx9sml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     (err) => {
         if(err){
-            console.log("Db not connected")
+            console.log("Database not connected", err)
         }else{
-            console.log("Db connected")
+            console.log("Database connected")
         }
     }
 )
@@ -25,6 +27,10 @@ app.use(cors())
 
 app.use('/auth', authRoute);
 
+app.use('/book', bookRoute)
+
+app.use('/crud', CRUDroute)
+
 
 app.get('/', (req, res) => {
     res.send("Hey World")
@@ -33,3 +39,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log("server is connected : ",port)
 })
+
