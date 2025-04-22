ba6af294c4a62d4b17862c7956e7c5343ba68f7e
ashritha0708 <axk67130@ucmo.edu>
2025-04-21 21:12:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,48 @@
 const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
 
-const app = express()
-const port = process.env.port || 8080
-const authRoute = require('./routes/auth-route')
-const bookRoute = require('./routes/booking-route')
-const CRUDroute = require('./routes/CRUD-route')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const authRoute = require('./routes/auth-route');
+const bookRoute = require('./routes/booking-route');
+const CRUDroute = require('./routes/CRUD-route');
 
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// MongoDB Atlas connection
 mongoose.connect(
     "mongodb+srv://ashritha97kommareddy:Ash13579@cluster0.ggx9sml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     (err) => {
-        if(err){
+        if (err) {
             console.log("Database not connected", err)
-        }else{
+        } else {
             console.log("Database connected")
         }
     }
-)
+);
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(cors())
+// CORS: allow frontend URL
+app.use(cors({
+  origin: 'https://7302e559.nosql1.pages.dev',
+  credentials: true,
+}));
 
+// Routes
 app.use('/auth', authRoute);
+app.use('/book', bookRoute);
+app.use('/crud', CRUDroute);
 
-app.use('/book', bookRoute)
-
-app.use('/crud', CRUDroute)
-
-
+// Base route
 app.get('/', (req, res) => {
     res.send("Hey World")
-})
+});
 
+// Start server
 app.listen(port, () => {
-    console.log("server is connected : ",port)
-})
-
+    console.log("server is connected : ", port)
+});
